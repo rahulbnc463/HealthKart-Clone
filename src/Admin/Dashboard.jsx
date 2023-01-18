b169bc5a866a194faf9e37b8c37d2eae0add3db2
rsrajput03 <rajputrohit9560@gmail.com>
2023-01-18 22:51:17 +0530

    navbar

---
+++ src/Admin/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { Routes, Route } from "react-router-dom";
+
+
+const Dashboard = (props) => {
+  return (
+    <div>
+      <Navbar/>
+ 
+      <br />
+     
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from "./Images/logo.png";
+// import logo from "./Images/logo.png";
 import "./App.css";
+import Dashboard from "./Admin/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is Our Logo</h1>
+        <h1>This is Our Logo</h1>
       <h1>Just demo Test</h1>
-      <img src={logo} alt="logo" />
+       <img src={logo} alt="logo" />   
+       {/* <Dashboard/>  */}
     </div>
   );
 }
